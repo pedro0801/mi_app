854a00e1d5449550843e0debdd20c645701997e2
Pedro Noe Mata Saucedo <86815090+pedro0801@users.noreply.github.com>
2025-07-22 12:27:55 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class MainWidget(BoxLayout):
+    pass
+
+class MiApp(App):
+    def build(self):
+        return MainWidget()
+
+if __name__ == '__main__':
+    MiApp().run()
